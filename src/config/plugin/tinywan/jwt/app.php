12e1ac38b5f2cedfdf492087457470871e96fc65
万少波 <756684177@qq.com>
2022-03-25 11:56:45 +0800

    修改默认配置access_exp

--- src/config/plugin/tinywan/jwt/app.php
+++ src/config/plugin/tinywan/jwt/app.php
@@ -7,12 +7,12 @@ return [
         'algorithms' => 'HS256',
         // access令牌秘钥
         'access_secret_key' => '2022d3d3LmJq',
-        // access令牌过期时间，单位秒。默认 2 小时
-        'access_exp' => 36000,
+        // access令牌过期时间，单位：秒。默认 2 小时
+        'access_exp' => 7200,
         // refresh令牌秘钥
         'refresh_secret_key' => '2022KTxigxc9o50c',
-        // refresh令牌过期时间，单位秒。默认 7 天
-        'refresh_exp' => 72000,
+        // refresh令牌过期时间，单位：秒。默认 7 天
+        'refresh_exp' => 604800,
         // 令牌签发者
         'iss' => 'webman.tinywan.cn',
         // 令牌签发时间
@@ -21,8 +21,8 @@ return [
         'leeway' => 60,
         // 单设备登录
         'is_single_device' => false,
-        // 缓存令牌时间，单位：秒
-        'cache_token_ttl' => 72000,
+        // 缓存令牌时间，单位：秒。默认 7 天
+        'cache_token_ttl' => 604800,
         // 缓存令牌前缀
         'cache_token_pre' => 'JWT:TOKEN:',
 
